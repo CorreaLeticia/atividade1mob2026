59628f1ba2ddd921be80782f09ee0af87860e2cd
CorreaLeticia <lele.150908@gmail.com>
2026-01-28 16:09:48 -0300

    Novo

---
+++ script.js
@@ -0,0 +1,8 @@
+async function obterDados(e){
+    const dolar = fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL")
+    .then(resp => resp.json())
+    .then(resp => {
+        e.innerHTML = resp.USDBRL.bid 
+    })
+
+}
